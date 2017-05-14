d7247608eab0e186433c9e0ba01eda44c9ef4c36
Sviatoslav Kovtunenko <sviatoslav.kovtunenko@re-flekt.com>
2017-05-14 14:26:45 +0200

    Add a reference to AWS SDK

--- app/models/dao/AdvertsDAO.scala
+++ app/models/dao/AdvertsDAO.scala
@@ -4,8 +4,21 @@ import models.Advert
 import org.joda.time.DateTime
 
 import scala.collection.mutable
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2._
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
+import play.api.libs.json._
+
 
 object AdvertsDAO {
+  val client = AmazonDynamoDBClientBuilder.standard()
+    .withRegion(Regions.EU_CENTRAL_1)
+    .build();
+  val dynamodb = new DynamoDB(client)
+  val tableName = "adverts"
+  val table = dynamodb.getTable(tableName)
+
   // Temporary in-memory repository
   var adverts = mutable.Buffer[Advert] (
     new Advert(1, "Audi A4", "gasoline", 10000, false, Option(100000), Option(DateTime.parse("2010"))),
@@ -15,7 +28,13 @@ object AdvertsDAO {
   )
 
   def add(advert: Advert) = {
-    adverts = adverts :+ advert
+    //adverts = adverts :+ advert
+    var advertJson = Json.toJson(advert).toString()
+    val item = new Item()
+      .withPrimaryKey("id", advert.id)
+      .withJSON("document", advertJson)
+    val putItemSpec = new PutItemSpec().withItem(item).withConditionExpression("attribute_not_exists(id)")
+    table.putItem(putItemSpec)
   }
 
   def edit(id: Int, advert: Advert) = {
@@ -41,6 +60,7 @@ object AdvertsDAO {
   }
 
   def get(id: Int): Option[Advert] = {
+    val asset = table.getItem("id", id)
     val existingAdvert = adverts.filter(_.id == id).lift(0)
     existingAdvert
   }
--- build.sbt
+++ build.sbt
@@ -4,7 +4,9 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.8"
+
+libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.11.127"
 
 libraryDependencies ++= Seq(
   jdbc,
@@ -14,6 +16,8 @@ libraryDependencies ++= Seq(
 )
 
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+resolvers +=
+  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
